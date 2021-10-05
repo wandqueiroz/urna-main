2ab016322595cf8d5c335732cfce798976bc9d04
Kaio <kaio.silveira@sdhds.fortaleza.ce.gov.br>
2021-10-05 16:36:44 -0300

    retirado 3 digitos

---
+++ js/etapas.js
@@ -0,0 +1,32 @@
+let etapas = [
+    {
+        titulo: 'SERVIDOR DESTAQUE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero:'40',
+                nome:'DR ZÉ LUIS',
+                setor:'TRANSPORTE',
+                fotos:[
+                    {url:'dr_ze_luis.jpg', legenda: 'SDHDS'},
+                ]
+            },
+            {
+                numero:'55',
+                nome:'ALDI BORGES',
+                setor:'COSAN',
+                fotos:[
+                    {url:'aldi_borges.jpg', legenda: 'SDHDS'},
+                ]
+            },
+            {
+                numero:'77',
+                nome:'JOEL DA PIZZARIA',
+                setor:'COTIC',
+                fotos:[
+                    {url:'joel.jpeg', legenda: 'SDHDS'},
+                ]
+            },
+        ]
+    }
+];
